b00942a35822258867cfd2f73def3df26c5fa753
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2024-10-31 07:40:28 +1300

    Javascript grin auto image slider

--- app.js
+++ app.js
@@ -6,3 +6,51 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+// initializeSlider()
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+
+
+function initializeSlider(){
+
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        setInterval(nextSlide, 4000);   
+    }
+       
+}
+
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    })
+
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+
+
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+
+}
